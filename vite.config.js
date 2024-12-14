96200020f0438b0a194c5bf69f1992103149b493
ayan-ali-khan <162182794+ayan-ali-khan@users.noreply.github.com>
2024-12-14 13:40:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],root: '.', // Default is the root of the project
+  plugins: [react()],
+  root: './', // Default is the root of the project
   build: {
     outDir: 'dist', // Ensure the output directory is correctly configured
   },
